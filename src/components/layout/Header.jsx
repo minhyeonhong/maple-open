a5903e04afe4cec3876493c795e55eccfcb7b2eb
minhyeonhong <mhh2029@gmail.com>
2024-01-05 09:20:53 +0900

    ENH : dark모드 테스트

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Header = () => {
+    const background_mode = (type) => {
+        const bodyStyle = document.body.style;
+        const theme = {
+            color: type === 'light' ? '#000' : '#FFF',
+            backgroundColor: type === 'light' ? '#FFF' : '#000',
+            border : type === 'light' ? '1px solid #000' : '1px solid #FFF'
+        };
+        bodyStyle.color = theme.color;
+        bodyStyle.backgroundColor = theme.backgroundColor;
+
+    }
+
     return (
         <div>
             Header
+            <div>
+                <button onClick={() => background_mode('light')}>light</button>
+                <button onClick={() => background_mode('dark')}>dark</button>
+            </div>
         </div>
     );
 };
